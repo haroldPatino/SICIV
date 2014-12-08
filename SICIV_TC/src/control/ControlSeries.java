568bbb7137b4b8bef267f04f5c4f3c62fd7967a7
Harold Patiño <harold.patino@uptc.edu.co>
2014-12-08 11:21:02 -0500

    Se genero la clase y los metodos para actualizacion e ingreso de productos serie al inventario

---
+++ SICIV_TC/src/control/ControlSeries.java
@@ -0,0 +1,228 @@
+/**
+ * 
+ */
+package control;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+import logica.Producto;
+import logica.ProductoSerie;
+import persistencia.ProductoDao;
+import persistencia.ProductoSerieDao;
+
+/**
+ * @author Harold Patiño
+ *
+ */
+public class ControlSeries implements Serializable{
+
+//Attributes---------------------------
+	private static final long serialVersionUID = 6534302942046890092L;
+	private String serial;
+	private String serialCarga;
+	private String nombreProducto;
+	private String estadoProducto;
+	private ProductoSerieDao dao;
+//Building-----------------------------
+	public ControlSeries(){
+		dao=new ProductoSerieDao();
+	}
+//Methods------------------------------
+	/**
+	 * @return serial
+	 */
+	public String getSerial() {
+		return serial;
+	}
+	/**
+	 * @return serialCarga
+	 */
+	public String getSerialCarga() {
+		return serialCarga;
+	}
+
+	/**
+	 * @param serialCarga serialCarga a colocar
+	 */
+	public void setSerialCarga(String serialCarga) {
+		this.serialCarga = serialCarga;
+	}
+
+	/**
+	 * @param serial serial a colocar
+	 */
+	public void setSerial(String serial) {
+		this.serial = serial;
+	}
+	/**
+	 * @return nombreProducto
+	 */
+	public String getNombreProducto() {
+		return nombreProducto;
+	}
+	/**
+	 * @param nombreProducto nombreProducto a colocar
+	 */
+	public void setNombreProducto(String nombreProducto) {
+		this.nombreProducto = nombreProducto;
+	}
+	/**
+	 * @return estadoProducto
+	 */
+	public String getEstadoProducto() {
+		return estadoProducto;
+	}
+	/**
+	 * @param estadoProducto estadoProducto a colocar
+	 */
+	public void setEstadoProducto(String estadoProducto) {
+		this.estadoProducto = estadoProducto;
+	}
+	/**
+	 * @return serialversionuid
+	 */
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	/**
+	 * @return dao
+	 */
+	public ProductoSerieDao getDao() {
+		return dao;
+	}
+	public Producto buscarProductoName(String name){
+		ProductoDao daoP=new ProductoDao();
+		ArrayList<Producto> list=daoP.selecionarProductos();
+		for(int i=0;i<list.size();i++){
+			Producto aux=list.get(i);
+			if(aux.getNombreProducto().toUpperCase().equals(name.toUpperCase())){
+				return aux;
+			}
+		}
+		return null;
+	}
+	public Producto buscarProductoId(int id){
+		ProductoDao daoP=new ProductoDao();
+		ArrayList<Producto> list=daoP.selecionarProductos();
+		for(int i=0;i<list.size();i++){
+			Producto aux=list.get(i);
+			if(aux.getIdProducto()==id){
+				return aux;
+			}
+		}
+		return null;
+	}
+	public void agregarProducto(ActionEvent actionEvent){
+	    FacesContext ctxtMsg = FacesContext.getCurrentInstance();
+		if(serial!="" && nombreProducto!="" && estadoProducto!=""){
+			ProductoSerie product=new ProductoSerie();
+			product.setNumeroSerie(serial);
+			product.setEstadoProducto(estadoProducto);
+			product.setIdFactura(0);
+			product.setIdProducto(buscarProductoName(nombreProducto).getIdProducto());
+			if(buscarSerie(product.getNumeroSerie())==null){
+				dao.insertarProductoSerie(product);
+				ctxtMsg.addMessage(null, new FacesMessage("Successful",  "Producto Agregado Exitosamente"));
+			}
+			else{
+				ctxtMsg.addMessage(null, new FacesMessage("Exception",  "El producto ya esta existente en el inventario"));
+			}
+		}
+		else{
+			ctxtMsg.addMessage(null, new FacesMessage("Error",  "Existen campos vacios"));
+
+		}
+	}
+	public void cargarProducto(ActionEvent actionEvent){
+	    FacesContext ctxtMsg = FacesContext.getCurrentInstance();
+	    ProductoSerie productSerie=buscarSerie(serialCarga);
+	    if(productSerie!=null){
+	    	serial=productSerie.getNumeroSerie();
+	    	nombreProducto=buscarProductoId(productSerie.getIdProducto()).getNombreProducto();
+	    	estadoProducto=manejoEstado(productSerie.getEstadoProducto());
+			ctxtMsg.addMessage(null, new FacesMessage("Successfull",  "Carga exitosa"));
+	    }
+	    else{
+			ctxtMsg.addMessage(null, new FacesMessage("Exception",  "El producto no esta registrado"));
+	    }
+	}
+	public void actualizarProducto(ActionEvent actionEvent){
+	    FacesContext ctxtMsg = FacesContext.getCurrentInstance();
+		if(serial!="" && nombreProducto!="" && estadoProducto!=""){
+			ProductoSerie product=new ProductoSerie();
+			product.setNumeroSerie(serial);
+			product.setEstadoProducto(manejoEstadoInverso(estadoProducto));
+			product.setIdFactura(0);
+			product.setIdProducto(buscarProductoName(nombreProducto).getIdProducto());
+			dao.actualizarDatosProductoSerie(product);
+			ctxtMsg.addMessage(null, new FacesMessage("Successful",  "Producto Actualizado Exitosamente"));
+		}
+		else{
+			ctxtMsg.addMessage(null, new FacesMessage("Error",  "Existen campos vacios"));
+
+		}
+	}
+	public ProductoSerie buscarSerie(String serie){
+		ArrayList<ProductoSerie> series=dao.selecionarProductosSerie();
+		for(int i=0;i<series.size();i++){
+			if(series.get(i).getNumeroSerie().toUpperCase().equals(serie.toUpperCase())){
+				return series.get(i);
+			}
+		}
+		return null;
+	}
+	public String manejoEstado(String est){
+		if(est.equals("AC")){
+			return "ACTIVO";
+		}
+		else if(est.equals("DV")){
+			return "DEVUELTO";
+		}
+		else if(est.equals("EP")){
+			return "EN PROCESO";
+		}
+		else if(est.equals("PE")){
+			return "PENDIENTE";
+		}
+		else{
+			return "";
+		}
+	}
+	public String manejoEstadoInverso(String est){
+		if(est.equals("ACTIVO")){
+			return "AC";
+		}
+		else if(est.equals("DEVUELTO")){
+			return "DV";
+		}
+		else if(est.equals("EN PROCESO")){
+			return "EP";
+		}
+		else if(est.equals("PENDIENTE")){
+			return "PE";
+		}
+		else{
+			return "";
+		}
+	}
+	public List<String> completeProductos(String text){
+		ArrayList<String> list=new ArrayList<String>();
+		list.add("ACTIVO");
+		list.add("DEVUELTO");
+		list.add("EN PROCESO");
+		list.add("PENDIENTE");
+		List<String> result=new ArrayList<String>();
+		for(int i=0;i<list.size();i++){
+			if(list.get(i).toUpperCase().contains(text.toUpperCase())){
+				result.add(list.get(i));
+			}
+		}
+		return result;
+	}
+}
